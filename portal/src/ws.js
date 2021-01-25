86138b86e58c4a6be82e25af94d03c6c94636006
Ian Dela Cruz <iandc76@gmail.com>
2021-01-25 08:38:20 +0800

    Add onError

--- portal/src/ws.js
+++ portal/src/ws.js
@@ -8,6 +8,7 @@ class WS {
     onConnected,
     onOrderChange,
     onDisconnect,
+    onError,
   }) {
     this.client = new W3CWebSocket(
       `${wsEndpoint}?companyId=${companyId}&outletId=${outletId}`
@@ -16,6 +17,7 @@ class WS {
     if (onConnected) this.client.onopen = onConnected;
     if (onOrderChange) this.client.onmessage = onOrderChange;
     if (onDisconnect) this.client.onclose = onDisconnect;
+    if (onError) this.client.onerror = onError;
   }
 
   disconnect = () => this.client.close();
